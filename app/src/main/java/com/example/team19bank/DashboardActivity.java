4d832584a354079e87df3177528064c8204c8fd2
oskar-saeberg <osb8@hi.is>
2026-02-25 00:22:28 +0000

    US3: Sýna account number og magn

--- app/src/main/java/com/example/team19bank/DashboardActivity.java
+++ app/src/main/java/com/example/team19bank/DashboardActivity.java
@@ -15,6 +15,9 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        TextView textAccountNumber = findViewById(R.id.textAccountNumber);
+        TextView textBalance = findViewById(R.id.textBalance);
+
         TextView textWelcome = findViewById(R.id.textWelcome);
         Button btnLogout = findViewById(R.id.btnLogout);
 
@@ -23,6 +26,23 @@ public class DashboardActivity extends AppCompatActivity {
         String user = sharedPref.getString("active_username", "User");
 
         textWelcome.setText("Welcome back, " + user + "!");
+
+        // US3: Loada account summary immediately
+        String accountNumber = sharedPref.getString("account_number", null);
+        long balanceISK = sharedPref.getLong("balance_isk", Long.MIN_VALUE);
+
+        // Demo defaults (þangað til backend er græjað)
+        if (accountNumber == null) {
+            accountNumber = "0130-26-000001";
+            sharedPref.edit().putString("account_number", accountNumber).apply();
+        }
+        if (balanceISK == Long.MIN_VALUE) {
+            balanceISK = 125000;
+            sharedPref.edit().putLong("balance_isk", balanceISK).apply();
+        }
+
+        textAccountNumber.setText("Account: " + accountNumber);
+        textBalance.setText("Balance: " + balanceISK + " ISK");
 
         // LOGOUT
         btnLogout.setOnClickListener(v -> {
